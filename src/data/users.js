b10d318dcbf43d3548c0c66f15906f33268300b9
JamAlves <jaymealves123@hotmail.com>
2021-05-15 21:10:01 -0300

    first commit

---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        name: 'Jayme Alves',
+        email: 'jamalves123@empmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2020/10/16/22/54/woman-5660786_960_720.png',
+         
+    
+    },
+    {
+        id: 2,
+        name: 'Julia Almeida',
+        email: 'juhall@empmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2020/10/16/22/54/woman-5660786_960_720.png',
+         
+    },
+    {
+        id: 3,
+        name: 'Beatriz Alves',
+        email: 'Biaalves@empmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2020/09/16/03/42/woman-5575199_960_720.png',
+         
+    },
+    {
+        id: 4,
+        name: 'Heloisa Alves',
+        email: 'loloalves@empmail.com',
+        avatarUrl:'https://cdn.pixabay.com/photo/2018/02/16/01/50/robot-3156763_960_720.png',
+        
+    },
+    ];
